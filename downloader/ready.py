8ceb1e57ba585ddc11e072c5b70c951e7bbb0fe6
SickAndTired <78124142+S1ckAndTired@users.noreply.github.com>
2023-01-08 22:53:41 -0300

    Update ready.py

--- downloader/ready.py
+++ downloader/ready.py
@@ -14,16 +14,19 @@ from downloader.dicts import *
 def xml():
     parser = argparse.ArgumentParser()
     parser = argparse.ArgumentParser(description="Created By: SickAndTired")
-    parser.add_argument("-t", "--target", metavar="\b", required=True, help="target.com")
+    parser.add_argument("-t", "--target", metavar="\b", required=True, help="some-target-bucket.s3-sa-east-1.amazonaws.com")
+    parser.add_argument("-x", "--extensions", metavar="\b", help="specify extensions separated by comma")
     parser.add_argument("-v", "--verbose", action="store_true")
-    parser.add_argument("any", nargs="*")
+    parser.add_argument(" ", nargs="*")
     args = parser.parse_args()
     target = args.target
     verbose = args.verbose
+    extensions = args.extensions.split(",")
     if verbose and target != "":
-        start2(target)
+        #print(extensions)
+        start2(target, extensions)
     elif target != "":
-        start(target)
+        start(target, extensions)
 
 def cct_fuzz():
     print("Soon!")
