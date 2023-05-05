cec8307c027db3eb3b9eb5fd24dd902d2a6fe250
SickAndTired <78124142+S1ckAndTired@users.noreply.github.com>
2023-05-05 08:26:59 -0300

    Update ready.py

--- downloader/ready.py
+++ downloader/ready.py
@@ -14,22 +14,22 @@ from downloader.dicts import *
 def xml():
     parser = argparse.ArgumentParser()
     parser = argparse.ArgumentParser(description="Created By: SickAndTired")
-    parser.add_argument("-t", "--target", metavar="\b", required=True, help="Pay the rent!")
-    parser.add_argument("-x", "--extensions", metavar="\b", help="Specify extensions separated by comma")
-    parser.add_argument("-ch", "--cheader", metavar="\b", help="Specify a custom headers")
+    parser.add_argument("-t", "--target", metavar="", required=True, help="Pay the rent!")
+    parser.add_argument("-e", "--extensions", metavar="", help="Specify extensions separated by comma")
+    parser.add_argument("-H", "--header", metavar="", help="Specify a custom header")
     parser.add_argument("-v", "--verbose", action="store_true")
-    parser.add_argument("-x", "--proxy", metavar="\b", help="Proxy - http://IP:PORT")
+    parser.add_argument("-x", "--proxy", metavar="", help="Proxy - http://IP:PORT")
     parser.add_argument(" ", nargs="*")
     args = parser.parse_args()
     target = args.target
     verbose = args.verbose
     extensions = args.extensions
-    cheader = args.cheader
+    header = args.header
     proxy = args.proxy
     if verbose and target != "":
-        start2(target, extensions, cheader, proxy)
+        start2(target, extensions, proxy, header)
     elif target != "":
-        start(target, extensions, cheader, proxy)xtensions)
+        start(target, extensions, proxy, header)
 
 def cct_fuzz():
     print("Soon!")
