d9aa60b75a82f3a53bf4993f140c1e005f1f596d
SickAndTired <78124142+S1ckAndTired@users.noreply.github.com>
2023-05-05 08:28:45 -0300

    Update writers.py

--- downloader/writers.py
+++ downloader/writers.py
@@ -4,6 +4,7 @@
 from progress.bar import Bar
 import requests
 import urllib3
+import time
 
 
 
@@ -12,22 +13,25 @@ from downloader.dicts import *
 
 
 requests.urllib3.disable_warnings()
-def download(url):
+def download(url, proxy, header):
     print(" [+] Process started!")
     bar = Bar (f" [+] Files downloaded",max=sum(ext_counts.values()), fill="█")
+    if proxy:
+        if url.startswith("https://"):
+            proxy = {"https": proxy}
+        elif url.startswith("http://"):
+            proxy = {"http": proxy}
+    if header:
+        header_name = header.split(": ")[0]
+        header_value = header.split(": ")[1]
+        headers = {header_name: header_value}
+    else:
+        headers = None
     for items in bigdata:
         print("\r    Working on it", end=""),bar.next()
         fille = items
         filename = fille.replace("/", "")
-        if proxy:
-            proxy = {"https": proxy}
-        if header:
-            header_name = header.split(": ")[0]
-            header_value = header.split(": ")[1]
-            headers = {header_name: header_value}
-        else:
-            headers = None   
-        r = requests.get(url+fille, verify=False, proxies=proxy, headers=headers)
+        r = requests.get(url+fille, headers=headers, verify=False, proxies=proxy)
         if r.status_code != 200:
             failedones[fille] = failedones.get(fille, 0) + 1
         else:
@@ -38,5 +42,6 @@ def download(url):
             else:
                 f = open(f"files/{filename}", "wb")
                 f.write(r.content)
-                f.close()
+                f.close()      
+
     bar.finish()
